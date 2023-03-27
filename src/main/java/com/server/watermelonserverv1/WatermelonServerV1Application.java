9ad87432ed97dc6be1c0bd24fa31ca4ed94f4385
luck1306 <luck1306@naver.com>
2023-03-27 12:23:23 +0900

    initial commit

---
+++ src/main/java/com/server/watermelonserverv1/WatermelonServerV1Application.java
@@ -0,0 +1,13 @@
+package com.server.watermelonserverv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermelonServerV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatermelonServerV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/watermelonserverv1/WatermelonServerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.watermelonserverv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermelonServerV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
