ce69a6450e5e3a2a3d6f15a0b980069bc84bd120
luck1306 <luck1306@naver.com>
2023-04-14 15:48:18 +0900

    UPDATE SecurityConfig

--- src/main/java/com/server/watermelonserverv1/global/security/SecurityConfig.java
+++ src/main/java/com/server/watermelonserverv1/global/security/SecurityConfig.java
@@ -1,2 +1,27 @@
-package com.server.watermelonserverv1.global.security;public class SecurityConfig {
+package com.server.watermelonserverv1.global.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 }
