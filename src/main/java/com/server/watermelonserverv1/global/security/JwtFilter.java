7473165393e30be5303d74e3e5cb8b5066fb45cc
luck1306 <luck1306@naver.com>
2023-04-15 23:39:33 +0900

    [UPDATE] resolveRequest {JwtFilter}

--- src/main/java/com/server/watermelonserverv1/global/security/JwtFilter.java
+++ src/main/java/com/server/watermelonserverv1/global/security/JwtFilter.java
@@ -1,2 +1,25 @@
-package com.server.watermelonserverv1.global.security;public class JwtFilter {
+package com.server.watermelonserverv1.global.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveRequest(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token.startsWith("Bearer ")) {
+            return token.substring(7);
+        }
+        return null;
+    }
 }
