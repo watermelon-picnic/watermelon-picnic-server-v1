cefbfdad1af2fc1f1c1e57d361def2ced4002296
luck1306 <luck1306@naver.com>
2023-04-16 04:42:58 +0900

    [UPDATE] add "role" filed {User}

--- src/main/java/com/server/watermelonserverv1/domain/user/domain/User.java
+++ src/main/java/com/server/watermelonserverv1/domain/user/domain/User.java
@@ -1,15 +1,19 @@
 package com.server.watermelonserverv1.domain.user.domain;
 
 import com.server.watermelonserverv1.domain.region.domain.Region;
+import com.server.watermelonserverv1.domain.user.domain.type.Role;
 import com.server.watermelonserverv1.global.entity.BasedIdEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -35,6 +39,11 @@ public class User extends BasedIdEntity {
 
     @Column(nullable = false)
     private String password;
+
+    @ColumnDefault("USER")
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @ManyToOne(targetEntity = Region.class)
     @JoinColumn(name = "region_id")
