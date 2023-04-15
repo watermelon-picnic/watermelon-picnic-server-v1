b89691be93235a897bb9c2c58f934043aaf799ba
luck1306 <luck1306@naver.com>
2023-04-15 23:25:49 +0900

    UPDATE Entity Filed User

--- src/main/java/com/server/watermelonserverv1/domain/user/domain/User.java
+++ src/main/java/com/server/watermelonserverv1/domain/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.server.watermelonserverv1.domain.user.domain;
 
+import com.server.watermelonserverv1.domain.region.domain.Region;
 import com.server.watermelonserverv1.global.entity.BasedIdEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,22 +10,36 @@ import lombok.NoArgsConstructor;
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @AttributeOverride(name = "id", column = @Column(name = "user_id"))
+@Table(
+uniqueConstraints = @UniqueConstraint(
+        name = "user_unq",
+        columnNames = {"email", "accountId"}
+    )
+)
 public class User extends BasedIdEntity {
 
-    @Column(nullable = false, length = 35)
+    @Column(nullable = false, length = 35, unique = true)
     private String email;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 20, unique = true)
     private String accountId;
 
     @Column(nullable = false)
     private String password;
 
+    @ManyToOne(targetEntity = Region.class)
+    @JoinColumn(name = "region_id")
+    private Region region;
+    
     @Builder
     public User(String email, String accountId, String password) {
         this.email = email;
