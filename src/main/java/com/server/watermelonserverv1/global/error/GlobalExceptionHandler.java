2de9d481fd53461f866f633652410d7c69025969
luck1306 <luck1306@naver.com>
2023-04-17 14:22:46 +0900

    [UPDATE] setting about WatermelonException {GlobalExceptionHandler}

--- src/main/java/com/server/watermelonserverv1/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/server/watermelonserverv1/global/error/GlobalExceptionHandler.java
@@ -1,2 +1,20 @@
-package com.server.watermelonserverv1.global.error;public class GlobalExceptionHandler {
+package com.server.watermelonserverv1.global.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({WatermelonException.class})
+    protected ResponseEntity<ErrorResponse> handlerWatermelonException(WatermelonException e) {
+        ErrorCode ex = e.getErrorCode();
+        return new ResponseEntity<>(ErrorResponse.builder()
+                .message(ex.getMessage())
+                .status(ex.getStatusCode())
+                .build(),
+                HttpStatus.valueOf(ex.getStatusCode()));
+    }
 }
