5c4a5c61e707df2432eb531a7d97d3280123f856
luck1306 <luck1306@naver.com>
2023-04-17 14:37:10 +0900

    [UPDATE] for default Security Config {JwtProvider}
    generateAuthentication 추가,
    encodingKey 추가

--- src/main/java/com/server/watermelonserverv1/global/security/JwtProvider.java
+++ src/main/java/com/server/watermelonserverv1/global/security/JwtProvider.java
@@ -1,2 +1,28 @@
-package com.server.watermelonserverv1.global.security;public class JwtProvider {
+package com.server.watermelonserverv1.global.security;
+
+import com.server.watermelonserverv1.global.security.details.DetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    @Value("${token.secret}")
+    private String secret;
+
+    private final DetailsService detailsService;
+
+    private final String encodingKey = Base64.getEncoder().encodeToString(secret.getBytes());
+
+    public Authentication generateAuthentication(String accountId) {
+        UserDetails details = detailsService.loadUserByUsername(accountId);
+        return new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+    }
 }
