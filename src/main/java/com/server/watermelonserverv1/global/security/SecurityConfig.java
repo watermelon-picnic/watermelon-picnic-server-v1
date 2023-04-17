cc93e22eeda3946a8518e92bac911b20d871bcd2
luck1306 <luck1306@naver.com>
2023-04-17 14:47:02 +0900

    [UPDATE] add filed JwtProvider {Securityconfig}

--- src/main/java/com/server/watermelonserverv1/global/security/SecurityConfig.java
+++ src/main/java/com/server/watermelonserverv1/global/security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.server.watermelonserverv1.global.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtProvider jwtProvider;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -22,6 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
-                .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
     }
 }
