94e7ce47b86e6418279f68622b7ba5fab7a6d5b1
luck1306 <luck1306@naver.com>
2023-04-17 14:42:52 +0900

    [UPDATE] add parseToken method {JwtProvider}

--- src/main/java/com/server/watermelonserverv1/global/security/JwtProvider.java
+++ src/main/java/com/server/watermelonserverv1/global/security/JwtProvider.java
@@ -1,6 +1,8 @@
 package com.server.watermelonserverv1.global.security;
 
 import com.server.watermelonserverv1.global.security.details.DetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,10 +21,18 @@ public class JwtProvider {
 
     private final DetailsService detailsService;
 
-    private final String encodingKey = Base64.getEncoder().encodeToString(secret.getBytes());
+    private final byte[] encodingKey = Base64.getEncoder().encodeToString(secret.getBytes()).getBytes();
 
     public Authentication generateAuthentication(String accountId) {
         UserDetails details = detailsService.loadUserByUsername(accountId);
         return new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+    }
+
+    public Claims parseToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(encodingKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
