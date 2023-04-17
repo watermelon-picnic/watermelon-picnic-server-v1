2983ec75a396d437534f765adc402fea6723cbd6
luck1306 <luck1306@naver.com>
2023-04-17 14:33:01 +0900

    [UPDATE] add findByAccountId method {UserRepository}

--- src/main/java/com/server/watermelonserverv1/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/server/watermelonserverv1/domain/user/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.server.watermelonserverv1.domain.user.domain.repository;
 import com.server.watermelonserverv1.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByAccountId(String accountId);
 }
