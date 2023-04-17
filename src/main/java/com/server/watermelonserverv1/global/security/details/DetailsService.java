38b0d8224eb1e0c5b9f6c3fc16f4980f8d9acf34
luck1306 <luck1306@naver.com>
2023-04-17 14:30:54 +0900

    [UPDATE] return Authentication {DetailsService}

--- src/main/java/com/server/watermelonserverv1/global/security/details/DetailsService.java
+++ src/main/java/com/server/watermelonserverv1/global/security/details/DetailsService.java
@@ -1,14 +1,23 @@
 package com.server.watermelonserverv1.global.security.details;
 
+import com.server.watermelonserverv1.domain.user.domain.User;
+import com.server.watermelonserverv1.domain.user.domain.repository.UserRepository;
+import com.server.watermelonserverv1.global.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class DetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByAccountId(username)
+                .orElseThrow(()->UserNotFoundException.EXCEPTION);
+        return new Details(user);
     }
 }
