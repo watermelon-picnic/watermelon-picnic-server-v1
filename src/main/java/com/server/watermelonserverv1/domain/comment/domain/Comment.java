cc28b97f1f44ef048a1bafc86d3afe3258da5a6b
luck1306 <luck1306@naver.com>
2023-04-20 12:13:42 +0900

    [CREATE] {Comment}

---
+++ src/main/java/com/server/watermelonserverv1/domain/comment/domain/Comment.java
@@ -0,0 +1,48 @@
+package com.server.watermelonserverv1.domain.comment.domain;
+
+import com.server.watermelonserverv1.domain.comment.domain.type.CommentType;
+import com.server.watermelonserverv1.domain.post.domain.Post;
+import com.server.watermelonserverv1.domain.writer.domain.Writer;
+import com.server.watermelonserverv1.global.entity.BasedIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "comment_id"))
+@Entity
+public class Comment extends BasedIdEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false, length = 15)
+    private CommentType commentType;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @JoinColumn(name = "post_id")
+    @ManyToOne
+    private Post post;
+
+    @JoinColumn(name = "writer_id")
+    @ManyToOne
+    private Writer writer;
+
+    @Builder
+    public Comment(CommentType commentType, String content, Post post, Writer writer) {
+        this.commentType = commentType;
+        this.content = content;
+        this.post = post;
+        this.writer = writer;
+    }
+}
