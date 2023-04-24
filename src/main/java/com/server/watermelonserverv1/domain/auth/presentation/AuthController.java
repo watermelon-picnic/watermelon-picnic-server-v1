f2e435d5d2d4f4bb9ce97b54d1b2fb78aa7a428e
luck1306 <luck1306@naver.com>
2023-04-24 14:33:09 +0900

    [UPDATE] define signUp method {AuthController}

--- src/main/java/com/server/watermelonserverv1/domain/auth/presentation/AuthController.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/presentation/AuthController.java
@@ -1,8 +1,20 @@
 package com.server.watermelonserverv1.domain.auth.presentation;
 
+import com.server.watermelonserverv1.domain.auth.presentation.dto.request.SignUpRequest;
+import com.server.watermelonserverv1.domain.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController("/auth")
 public class AuthController {
-    
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public SignUpRequest signUp(@RequestBody SignUpRequest request) {
+        authService.signUp(request);
+    }
 }
