ad9016a57c746ac14c6d6c01ed2e9f976d947af6
luck1306 <luck1306@naver.com>
2023-04-24 14:14:11 +0900

    [CREATE] {AuthController}

---
+++ src/main/java/com/server/watermelonserverv1/domain/auth/presentation/AuthController.java
@@ -0,0 +1,8 @@
+package com.server.watermelonserverv1.domain.auth.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/auth")
+public class AuthController {
+    
+}
