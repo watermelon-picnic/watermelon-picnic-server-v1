16e9f13ed3a9299bad1c7d94c71dbec238315527
luck1306 <luck1306@naver.com>
2023-04-24 14:30:57 +0900

    [UPDATE] define signUp method {AuthService}

--- src/main/java/com/server/watermelonserverv1/domain/auth/service/AuthService.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/service/AuthService.java
@@ -1,7 +1,12 @@
 package com.server.watermelonserverv1.domain.auth.service;
 
+import com.server.watermelonserverv1.domain.auth.presentation.dto.request.SignUpRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    public void signUp(SignUpRequest request) {
+        //
+    }
 }
