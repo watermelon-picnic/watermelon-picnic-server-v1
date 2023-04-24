d6a481b0124ef6322bf77bc35eba2bc8ea071974
luck1306 <luck1306@naver.com>
2023-04-24 23:10:56 +0900

    [UPDATE] {ExistEmailException, ErrorCode}
    ErrorCode : ExistEmailException을 위한 상수 추가
    ExistEmailException : 이미 존재하는 Email을 처리하기 위한 예외

--- src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
+++ src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    USER_NOT_FOUND("사용자 정보를 찾을 수 없습니다.", 404);
+    USER_NOT_FOUND("사용자 정보를 찾을 수 없습니다.", 404),
+    EXIT_EMAIL_PRECONDITION_FAILED("이미 존재하는 이메일 입니다", 412);
 
     private final String message;
     private final int statusCode;
