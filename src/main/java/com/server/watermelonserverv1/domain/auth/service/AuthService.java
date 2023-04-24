d863146911e67ddb1cb0f660b76c8a868f6db1cf
luck1306 <luck1306@naver.com>
2023-04-24 23:15:37 +0900

    [UPDATE] signUp method implement {AuthService}

--- src/main/java/com/server/watermelonserverv1/domain/auth/service/AuthService.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/service/AuthService.java
@@ -1,12 +1,26 @@
 package com.server.watermelonserverv1.domain.auth.service;
 
+import com.server.watermelonserverv1.domain.auth.exception.ExistEmailException;
 import com.server.watermelonserverv1.domain.auth.presentation.dto.request.SignUpRequest;
+import com.server.watermelonserverv1.domain.user.domain.User;
+import com.server.watermelonserverv1.domain.user.domain.repository.UserRepository;
+import com.server.watermelonserverv1.domain.user.domain.type.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class AuthService {
 
+    private final UserRepository userRepository;
+
     public void signUp(SignUpRequest request) {
-        //
+        User dbInfo = userRepository.findByEmail(request.getEmail()).orElse(null);
+        if (dbInfo != null) throw ExistEmailException.EXCEPTION;
+        userRepository.save(User.builder()
+                        .email(request.getEmail())
+                        .password(request.getPassword())
+                        .role(Role.USER)
+                .build());
     }
 }
