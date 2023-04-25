022747db903cf140ca9ed6564617d1b2e0c0eae4
luck1306 <luck1306@naver.com>
2023-04-25 17:08:10 +0900

    [CREATE] {TokenNotFoundException, ErrorCode}
    TokenNotFoundException : 지정한 아이디를 식별자로 저장된 정보가 존재하지 않음을 나타내는 예외
    ErrorCode : TokenNotFoundException을 위한 상수 추가

--- src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
+++ src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ErrorCode {
     USER_NOT_FOUND("사용자 정보를 찾을 수 없습니다.", 404),
     PASSWORD_INCORRECT("비밀번호가 일치하지 않습니다.", 400),
+    TOKEN_NOT_FOUND("해당 아이디로 저장된 token 정보가 존재하지 않습니다.", 404),
     EXIT_EMAIL_PRECONDITION_FAILED("이미 존재하는 이메일 입니다", 412);
 
     private final String message;
