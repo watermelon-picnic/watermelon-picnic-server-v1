0eb469068078a80f7311ca2023f4cb6711456221
luck1306 <luck1306@naver.com>
2023-04-25 09:01:58 +0900

    [UPDATE] add PasswordEncoder {SecurityConfig}

--- src/main/java/com/server/watermelonserverv1/global/security/SecurityConfig.java
+++ src/main/java/com/server/watermelonserverv1/global/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.server.watermelonserverv1.global.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
@@ -12,6 +15,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtProvider jwtProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
