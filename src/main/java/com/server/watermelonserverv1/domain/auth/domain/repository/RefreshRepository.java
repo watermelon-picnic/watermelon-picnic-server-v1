f9c1c6b62f48fa4b4bd21002d78dbb39b3c6b81d
luck1306 <luck1306@naver.com>
2023-04-25 16:53:09 +0900

    [CREATE] {RefreshRepository}

---
+++ src/main/java/com/server/watermelonserverv1/domain/auth/domain/repository/RefreshRepository.java
@@ -0,0 +1,7 @@
+package com.server.watermelonserverv1.domain.auth.domain.repository;
+
+import com.server.watermelonserverv1.domain.auth.domain.Refresh;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshRepository extends CrudRepository<Refresh, Long> {
+}
