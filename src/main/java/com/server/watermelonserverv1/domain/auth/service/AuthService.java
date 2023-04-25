3247e96126853bcfb3c29a0c896e6842ba4eecf2
luck1306 <luck1306@naver.com>
2023-04-25 09:54:43 +0900

    [UPDATE] define login method {AuthService}

--- src/main/java/com/server/watermelonserverv1/domain/auth/service/AuthService.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/service/AuthService.java
@@ -1,11 +1,16 @@
 package com.server.watermelonserverv1.domain.auth.service;
 
 import com.server.watermelonserverv1.domain.auth.exception.ExistEmailException;
+import com.server.watermelonserverv1.domain.auth.exception.PasswordIncorrectException;
 import com.server.watermelonserverv1.domain.auth.presentation.dto.request.SignUpRequest;
+import com.server.watermelonserverv1.domain.auth.presentation.dto.response.TokenResponse;
 import com.server.watermelonserverv1.domain.user.domain.User;
 import com.server.watermelonserverv1.domain.user.domain.repository.UserRepository;
 import com.server.watermelonserverv1.domain.user.domain.type.Role;
+import com.server.watermelonserverv1.global.exception.UserNotFoundException;
+import com.server.watermelonserverv1.global.security.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,6 +18,10 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtProvider jwtProvider;
 
     public void signUp(SignUpRequest request) {
         User dbInfo = userRepository.findByEmail(request.getEmail()).orElse(null);
@@ -22,5 +31,16 @@ public class AuthService {
                         .password(request.getPassword())
                         .role(Role.USER)
                 .build());
+    }
+
+    public TokenResponse login(SignUpRequest request) {
+        User dbInfo = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(()->UserNotFoundException.EXCEPTION);
+        if (!passwordEncoder.matches(request.getPassword(), dbInfo.getPassword()))
+            throw PasswordIncorrectException.EXCEPTION;
+        return TokenResponse.builder()
+                .accessToken(jwtProvider.accessTokenGenerator(dbInfo.getEmail()))
+                .refreshToken(jwtProvider.refreshTokenGenerator(dbInfo))
+                .build();
     }
 }
