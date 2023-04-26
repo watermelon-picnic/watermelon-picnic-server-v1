82865c7a6ce13d2d6730b5fc778efcfbe35dc855
luck1306 <luck1306@naver.com>
2023-04-27 00:35:04 +0900

    [CREATE] {TokenExpiredException, ErrorCode}
    TokenExpiredException : 토큰 만료를 위한 예외
    ErrorCode : 상수 추가

--- src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
+++ src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
@@ -7,6 +7,7 @@ public enum ErrorCode {
     USER_NOT_FOUND("사용자 정보를 찾을 수 없습니다.", 404),
     PASSWORD_INCORRECT("비밀번호가 일치하지 않습니다.", 400),
     TOKEN_NOT_FOUND("해당 아이디로 저장된 token 정보가 존재하지 않습니다.", 404),
+    TOKEN_EXPIRED("토큰이 만료되었습니다.", 403),
     EXIT_EMAIL_PRECONDITION_FAILED("이미 존재하는 이메일 입니다", 412);
 
     private final String message;
