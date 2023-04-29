9e4b1e5fd29e023dd5e5aef24db46b1efba82c21
luck1306 <luck1306@naver.com>
2023-04-30 00:36:11 +0900

    [UPDATE] {AuthCotroller, SignUpRequest}
    AuthContrller : add Valid annotation in signUp, login method
    SignUpRequest : add contraint

--- src/main/java/com/server/watermelonserverv1/domain/auth/presentation/AuthController.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/presentation/AuthController.java
@@ -5,11 +5,16 @@ import com.server.watermelonserverv1.domain.auth.presentation.dto.response.Token
 import com.server.watermelonserverv1.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -19,13 +24,19 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/sign-up")
-    public void signUp(@RequestBody SignUpRequest request) { authService.signUp(request); }
+    public void signUp(@Valid @RequestBody SignUpRequest request) { authService.signUp(request); }
 
     @PostMapping("/login")
-    public TokenResponse login(@RequestBody SignUpRequest request) { return authService.login(request); }
+    public TokenResponse login(@Valid @RequestBody SignUpRequest request) { return authService.login(request); }
 
     @DeleteMapping("/logout")
     public void logout() { authService.logout(); }
+
+    // this api will erase
+    @GetMapping("/{email}")
+    public boolean validationEmail(@PathVariable String email) {
+        return authService.validationEmail(email);
+    }
 
     @PutMapping("/reissue")
     public TokenResponse reissue() { return authService.reissue(); }
--- src/main/java/com/server/watermelonserverv1/domain/auth/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/presentation/dto/request/SignUpRequest.java
@@ -2,15 +2,22 @@ package com.server.watermelonserverv1.domain.auth.presentation.dto.request;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
 public class SignUpRequest {
 
-    @Size(min = 0, max = 35)
+    @Size(min = 8, max = 35, message = "please follow email size(8~35) requirement")
+    @NotBlank(message = "please insert any value in email request")
+    @Pattern(
+            regexp = "[0-9a-zA-Z]+@[a-z]+\\.[a-z]+(.[a-z]+)?",
+            message = "email is not matched"
+    )
     private String email;
 
-    @Size(min = 8, max = 255)
+    @NotBlank(message = "please insert any value in password request")
+    @Size(min = 8, max = 255, message = "please follow password size(8~255) requirement")
     private String password;
-    
 }
