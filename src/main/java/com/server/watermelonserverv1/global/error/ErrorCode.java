2843219410328605684086c4cc546dc240420ac1
luck1306 <luck1306@naver.com>
2023-04-30 21:06:08 +0900

    [UPDATE] {BirthBadRequestException, ErrorCode}
    BirthBadRequestException : ParseException 대신 핸들링 하기 위한 예외입니다. DateFormat.parse() method는 조건부로 ParseException 예외를 발생시키기 때문에 다음과 같은 예외 핸들러 클래스를 생성했습니다.
    ErrorCode : BirthBadRequestException을 위한 상수 추가

--- src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
+++ src/main/java/com/server/watermelonserverv1/global/error/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     TOKEN_EXPIRED("토큰이 만료되었습니다.", 403),
     AUTH_BAD_REQUEST("인증을 위해 토큰을 동봉해 주십시오.", 400),
     EXIT_EMAIL_PRECONDITION_FAILED("이미 존재하는 이메일입니다", 400),
-    EXIT_NICKNAME_PRECONDITION_FAILED("이미 존재하는 닉네임입니다.", 400);
+    EXIT_NICKNAME_PRECONDITION_FAILED("이미 존재하는 닉네임입니다.", 400),
+    BIRTH_BAD_REQUEST("잘못된 생년월일입니다.", 400);
 
     private final String message;
     private final int statusCode;
