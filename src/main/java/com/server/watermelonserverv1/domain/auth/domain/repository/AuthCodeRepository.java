7ed0f0cd58a1a20f0f5266a6252ec4103f581c0d
luck1306 <luck1306@naver.com>
2023-05-01 11:15:51 +0900

    [UPDATE] Generic Type change {AuthCodeRepository}

--- src/main/java/com/server/watermelonserverv1/domain/auth/domain/repository/AuthCodeRepository.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/domain/repository/AuthCodeRepository.java
@@ -3,5 +3,5 @@ package com.server.watermelonserverv1.domain.auth.domain.repository;
 import com.server.watermelonserverv1.domain.auth.domain.AuthCode;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AuthCodeRepository extends CrudRepository<AuthCode, Long> {
+public interface AuthCodeRepository extends CrudRepository<AuthCode, String> {
 }
