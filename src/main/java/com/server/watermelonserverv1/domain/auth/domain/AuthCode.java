6513c32664dcc53417d2f07906b083a075ac608c
luck1306 <luck1306@naver.com>
2023-05-01 11:14:51 +0900

    [UPDATE] change import class {AuthCode, Refresh}

--- src/main/java/com/server/watermelonserverv1/domain/auth/domain/AuthCode.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/domain/AuthCode.java
@@ -5,11 +5,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.Id;
 import java.util.concurrent.TimeUnit;
 
 @Getter
--- src/main/java/com/server/watermelonserverv1/domain/auth/domain/Refresh.java
+++ src/main/java/com/server/watermelonserverv1/domain/auth/domain/Refresh.java
@@ -4,11 +4,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
