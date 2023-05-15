eff29fe1b7275a0882388725d34a50ed34017102
luck1306 <luck1306@naver.com>
2023-05-16 08:59:19 +0900

    [CREATE] {AuthPostController, AnonymusPostController}

---
+++ src/main/java/com/server/watermelonserverv1/domain/post/presentation/AuthPostController.java
@@ -0,0 +1,11 @@
+package com.server.watermelonserverv1.domain.post.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/post/auth")
+@RestController
+public class AuthPostController {
+}
