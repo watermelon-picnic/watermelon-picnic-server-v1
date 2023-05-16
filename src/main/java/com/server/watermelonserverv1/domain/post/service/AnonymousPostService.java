cbc5d73d088771f734092e4ed044d8555764154a
luck1306 <luck1306@naver.com>
2023-05-16 09:01:18 +0900

    [CREATE] {AuthPostService, AnnonymousService}

---
+++ src/main/java/com/server/watermelonserverv1/domain/post/service/AnonymousPostService.java
@@ -0,0 +1,9 @@
+package com.server.watermelonserverv1.domain.post.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AnonymousPostService {
+}
